a96d52977514e74274df754f6342fcc2975d983e
Mirzokhid522 <victorsamraz1999@gmail.com>
2026-09-12 19:51:05 +0300

    Update app.py with Selenium and configure Docker timeout

--- app.py
+++ app.py
@@ -163,5 +163,4 @@ def index():
     return render_template("index.html", banner=banner, banner_sub=banner_sub, meetings=meetings, matrix_headers=matrix_headers, matrix_rows=matrix_rows)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, port=5000)
